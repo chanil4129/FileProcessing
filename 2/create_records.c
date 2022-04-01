21aa72a6c61f523fc7213777dc929c657f90fbf6
chan <chanil4129@naver.com>
2022-04-01 21:30:43 +0900

    project2 modify

--- 2/create_records.c
+++ 2/create_records.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
-#include "student.h"
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+typedef struct Student{
+    char name[15];
+    char age[5];
+    char address[20];
+    char info[160];
+} Student;
 
 int main(int argc, char **argv){
     Student student;
