e80d2048c04f67a6f3f5093c212c791f4b98d873
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2022-06-17 16:45:06 +0900

    add

--- 5/student.c
+++ 5/student.c
@@ -1,4 +1,9 @@
 #include <stdio.h>		// 필요한 header file 추가 가능
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "student.h"
 
 //
@@ -13,7 +18,7 @@ void unpack(const char *recordbuf, STUDENT *s);
 //
 // 함수 writeRecord()는 학생 레코드 파일에 주어진 rrn에 해당하는 위치에 recordbuf에 
 // 저장되어 있는 레코드를 저장한다. 이전에 pack() 함수를 호출하여 recordbuf에 데이터를 채워 넣는다.
-// 성공적으로 수행하면 '1'을, 그aa렇지 않으면 '0'을 리턴한다.
+// 성공적으로 수행하면 '1'을, 그렇지 않으면 '0'을 리턴한다.
 //
 int writeRecord(FILE *fp, const STUDENT *s, int rrn);
 void pack(char *recordbuf, const STUDENT *s);
@@ -45,12 +50,210 @@ void printRecord(const STUDENT *s);
 //
 enum FIELD getFieldID(char *fieldname);
 
-void main(int argc, char *argv[])
-{
+
+//
+// 학생 레코드 파일에서 "학번" 키값을 만족하는 레코드를 찾아서 이것을 삭제한다.
+// 참고로, 검색 조건은 반드시 학번(ID)만을 사용한다. 레코드의 검색은 searchRecord() 함수와
+// 유사한 방식으로 구현한다. 성공적으로 수행하면 '1'을, 그렇지 않으면 '0'을 리턴한다.
+//
+int deleteRecord(FILE *fp, enum FIELD f, char *keyval);
+
+//
+// 학생 레코드 파일에 새로운 레코드를 추가한다. 과제 설명서에서 언급한 대로, 삭제 레코드가
+// 존재하면 반드시 삭제 레코드들 중 하나에 새로운 레코드를 저장한다. 삭제 레코드 리스트 관리와
+// 삭제 레코드의 선택은 교재 방식을 따른다. 새로운 레코드의 추가는 appendRecord() 함수와 비슷한
+// 방식으로 구현한다. 성공적으로 수행하면 '1'을, 그렇지 않으면 '0'을 리턴한다.
+//
+int insertRecord(FILE *fp, char *id, char *name, char *dept, char *addr, char *email);
+
+
+void main(int argc, char *argv[]){
 	FILE *fp;			// 모든 file processing operation은 C library를 사용할 것
+	enum FIELD field;
+
+	if(strcmp(argv[1],"-a")&&strcmp(argv[1],"-s")){
+		fprintf(stderr,"usage: %s <option> ...\n",argv[0]);
+		exit(1);
+	}
+	
+	if(!strcmp(argv[1],"-a")&&argc!=8){
+		fprintf(stderr,"usage: %s -a <record_file_name> \"field_value1\" \"field_value2\" \"field_value3\" \"field_value4\" \"field_value5\"\n",argv[0]);
+		exit(1);
+	}
+
+	if(!strcmp(argv[1],"-s")&&argc!=4){
+		fprintf(stderr,"usage: %s -s <record_file_name> \"field_name=field_value\"\n",argv[0]);
+		exit(1);
+	}
+
+	if(!strcmp(argv[1],"-a")){
+		if((fp=fopen(argv[2],"r+"))==NULL){
+			if((fp=fopen(argv[2],"w+"))==NULL){
+				fprintf(stderr,"fopen error\n");
+				exit(1);
+			}
+		}
+		if(!appendRecord(fp,argv[3],argv[4],argv[5],argv[6],argv[7])){
+			printf("appendRecord error\n");
+			exit(1);
+		}
+	}
+
+	if(!strcmp(argv[1],"-s")){
+		if((fp=fopen(argv[2],"r+"))==NULL){
+			fprintf(stderr,"fopen error for %s\n",argv[2]);
+			exit(1);
+		}
+		field=getFieldID(argv[3]);
+		searchRecord(fp,field,argv[3]);
+	}
+
+	fclose(fp);
+	exit(0);
+}
+int readRecord(FILE *fp, STUDENT *s, int rrn){
+	char recordbuf[RECORD_SIZE];
+	long offset;
+
+	offset=rrn*RECORD_SIZE+HEADER_SIZE;
+	if(fseek(fp,offset,SEEK_SET)!=0){
+		fprintf(stderr,"seek error\n");
+		return 0;
+	}
+	if(fread(recordbuf,sizeof(recordbuf),1,fp)!=1){
+		fprintf(stderr,"read error\n");
+		return 0;
+	}
+	unpack(recordbuf,s);
+	return 1;
 }
 
-void printRecord(const STUDENT *s)
-{
+void unpack(const char *recordbuf, STUDENT *s){
+	memcpy(s->id,recordbuf,IL);
+	s->id[IL]='\0';
+	memcpy(s->name,recordbuf+1+IL,NL);
+	s->name[NL]='\0';
+	memcpy(s->dept,recordbuf+2+IL+NL,DL);
+	s->dept[DL]='\0';
+	memcpy(s->addr,recordbuf+3+IL+NL+DL,AL);
+	s->addr[AL]='\0';
+	memcpy(s->email,recordbuf+4+IL+NL+DL+AL,EL);
+	s->email[EL]='\0';
+}
+
+int writeRecord(FILE *fp, const STUDENT *s, int rrn){
+	char recordbuf[RECORD_SIZE];
+	long offset;
+
+	pack(recordbuf,s);
+	offset=HEADER_SIZE+rrn*RECORD_SIZE;
+	if(fseek(fp,offset,SEEK_SET)!=0){
+		fprintf(stderr,"seek error\n");
+		return 0;
+	}
+	if(fwrite(recordbuf,sizeof(recordbuf),1,fp)!=1){
+		fprintf(stderr,"write error\n");
+		return 0;
+	}
+
+	return 1;
+}
+
+void pack(char *recordbuf, const STUDENT *s){
+	memcpy(recordbuf,s->id,IL);
+	memcpy(recordbuf+IL,"#",1);
+	memcpy(recordbuf+IL+1,s->name,NL);
+	memcpy(recordbuf+IL+NL+1,"#",1);
+	memcpy(recordbuf+IL+NL+2,s->dept,DL);
+	memcpy(recordbuf+IL+NL+DL+2,"#",1);
+	memcpy(recordbuf+IL+NL+DL+3,s->addr,AL);
+	memcpy(recordbuf+IL+NL+DL+AL+3,"#",1);
+	memcpy(recordbuf+IL+NL+DL+AL+4,s->email,EL);
+	memcpy(recordbuf+IL+NL+DL+AL+EL+4,"#",1);
+}
+
+int appendRecord(FILE *fp, char *id, char *name, char *dept, char *addr, char *email){
+	STUDENT s;
+	char headbuf[HEADER_SIZE];
+	char recordbuf[RECORD_SIZE];
+	int rrn;
+	int fd;
+	char *ptr=NULL;
+
+	fd=fileno(fp);
+	//header 정보 넣기
+	if(lseek(fd,0,SEEK_END)==0){
+		strcpy(headbuf,"1");
+		rrn=0;
+	}
+	else {
+		lseek(fd,0,SEEK_SET);
+		read(fd,headbuf,HEADER_SIZE);
+		rrn=atoi(headbuf);
+		sprintf(headbuf,"%d",rrn+1);
+	}
+	lseek(fd,0,SEEK_SET);
+	write(fd,headbuf,HEADER_SIZE);
+
+	//구조체에 데이터 삽입
+	strcpy(s.id,id);
+	strcpy(s.name,name);
+	strcpy(s.dept,dept);
+	strcpy(s.addr,addr);
+	strcpy(s.email,email);
+		
+	//record에 넣기
+	if(!writeRecord(fp,&s,rrn)){
+		printf("writeRecord error\n");
+		return 0;
+	}
+	return 1;
+}
+
+void searchRecord(FILE *fp, enum FIELD f, char *keyval){
+	STUDENT s;
+	char recordbuf[RECORD_SIZE];
+	int rrn=0;
+	int eof;
+	int fd;
+
+	keyval=strstr(keyval,"=")+1;
+	fd=fileno(fp);
+	eof=lseek(fd,0,SEEK_END);
+	lseek(fd,0,SEEK_SET);
+	for(int i=HEADER_SIZE;i<eof;i+=RECORD_SIZE){
+		if(!readRecord(fp,&s,rrn)){
+			printf("none data\n");
+			return;
+		}
+		if(f==ID&&!strcmp(s.id,keyval))
+			printRecord(&s);
+		if(f==NAME&&!strcmp(s.name,keyval))
+			printRecord(&s);
+		if(f==DEPT&&!strcmp(s.dept,keyval))
+			printRecord(&s);
+		if(f==ADDR&&!strcmp(s.addr,keyval))
+			printRecord(&s);
+		if(f==EMAIL&&!strcmp(s.email,keyval))
+			printRecord(&s);
+		rrn++;
+	}
+}
+
+enum FIELD getFieldID(char *fieldname){
+	char *ptr=NULL;
+	if((ptr=strstr(fieldname,"ID"))!=NULL)
+		return ID;
+	if((ptr=strstr(fieldname,"NAME"))!=NULL)
+		return NAME;
+	if((ptr=strstr(fieldname,"DEPT"))!=NULL)
+		return DEPT;
+	if((ptr=strstr(fieldname,"ADDR"))!=NULL)
+		return ADDR;
+	if((ptr=strstr(fieldname,"EMAIL"))!=NULL)
+		return EMAIL;
+}
+
+void printRecord(const STUDENT *s){
 	printf("%s | %s | %s | %s | %s\n", s->id, s->name, s->dept, s->addr, s->email);
 }
